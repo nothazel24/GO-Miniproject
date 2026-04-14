8c02af242d67d6d8760a7f8e1ffdd7a15242ce16
nothazel24 <frdnryann@gmail.com>
2026-04-14 13:57:13 +0700

    add: divide validation & panic handling

--- exercise/calculator.go
+++ exercise/calculator.go
@@ -1,6 +1,9 @@
 package exercise
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 var operasi = map[string]func(int, int) int{
 	"+": func(i, j int) int { return i + j },
@@ -9,21 +12,42 @@ var operasi = map[string]func(int, int) int{
 	"/": func(i, j int) int { return i / j },
 }
 
+func divideZeroWarn(j int, operator string) (bool, error) {
+	if j == 0 && operator == "/" {
+		return false, errors.New("Tidak bisa dibagi 0!")
+	}
+
+	return true, nil
+}
+
 func Calculator() {
 	var i, j int
 	var operator string
 
-	fmt.Println("Masukkan nomor pertama")
+	defer func() {
+		if r := recover(); r != nil {
+			fmt.Println("[System] Terjadi kesalahan :", r)
+		}
+	}()
+
+	fmt.Print("Masukkan nomor pertama : ")
 	fmt.Scanln(&i)
 
-	fmt.Println("Masukkan nomor kedua")
+	fmt.Print("Masukkan nomor kedua : ")
 	fmt.Scanln(&j)
 
-	fmt.Println("Pilih Operator (+, -, /, *):")
+	fmt.Print("Pilih Operator (+, -, /, *) : ")
 	fmt.Scanln(&operator)
 
 	// checking operator
 	if op, ok := operasi[operator]; ok {
+
+		// validasi sebelum terjadi panic
+		if valid, err := divideZeroWarn(j, operator); !valid {
+			fmt.Println("Error :", err)
+			return
+		}
+
 		hasil := op(i, j)
 		fmt.Println("Hasil:", hasil)
 	} else {
